95f95113e6ff9c15739cef13299644d2349e03ab
hajindvlp <harryhajinchung@gmail.com>
2020-07-28 18:17:42 +0900

    v.0.3

--- src/popup.js
+++ src/popup.js
@@ -1,89 +1,77 @@
-var d = new Date();
-var oldMeal;
-var oldDate;
-
-var date =  `${String(d.getFullYear())}-${String(d.getMonth()+1)}-${String(d.getDate())}`;
-var url = `https://dev-api.dimigo.in/dimibobs/${date}`;
-
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false );
+const httpGet = (endpoint) => {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", endpoint, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
 
-// if(oldDate == date) {
-//     document.getElementById("breakfast").innerHTML = oldMeal.breakfast;
-//     document.getElementById("lunch").innerHTML = oldMeal.lunch;
-//     document.getElementById("dinner").innerHTML = oldMeal.dinner;
-//     document.getElementById("date").innerHTML = date;    
-// }
-
-if(true) {
-    var mealStr = httpGet(url);
-    var meal = JSON.parse(mealStr);
-
-    var breakfast = [], breakfastHTML = "";
-    breakfast = meal.breakfast.split('/');
-    for(menu in breakfast)
-        breakfastHTML += `<li>${breakfast[menu]}</li>`;
-    var lunch = [], lunchHTML = "";
-    lunch = meal.lunch.split('/');
-    for(menu in lunch)
-        lunchHTML += `<li>${lunch[menu]}</li>`;
-    var dinner = [], dinnerHTML = "";
-    dinner = meal.dinner.split('/');
-    for(menu in dinner)
-        dinnerHTML += `<li>${dinner[menu]}</li>`;
-
-    document.getElementById("breakfast").innerHTML = breakfastHTML;
-    document.getElementById("lunch").innerHTML = lunchHTML;
-    document.getElementById("dinner").innerHTML = dinnerHTML;
-    document.getElementById("date").innerHTML = date;   
-
-    oldDate = date;
-    oldMeal = meal;
+const loadMeal = (date) => {
+    let mealStr = httpGet(`https://dev-api.dimigo.in/dimibobs/${date}`);
+    let meal = JSON.parse(mealStr);
+    
+    if(meal) {
+        // let breakfast = [], breakfastHTML = "";
+        // breakfast = meal.breakfast.split('/');
+        // for(menu in breakfast)
+        //     breakfastHTML += `<li>${breakfast[menu]}</li>`;
+        // let lunch = [], lunchHTML = "";
+        // lunch = meal.lunch.split('/');
+        // for(menu in lunch)
+        //     lunchHTML += `<li>${lunch[menu]}</li>`;
+        // let dinner = [], dinnerHTML = "";
+        // dinner = meal.dinner.split('/');
+        // for(menu in dinner)
+        //     dinnerHTML += `<li>${dinner[menu]}</li>`;
+    
+        document.getElementById("breakfast").innerHTML = meal.breakfast;
+        document.getElementById("lunch").innerHTML = meal.lunch;
+        document.getElementById("dinner").innerHTML = meal.dinner;
+    } 
+    document.getElementById("date").innerHTML = date;  
 }
 
-var zoomRate = 100;
+let zoomRate = 100;
 
-function zoomIn() {
-    var Page = document.getElementById('Body');
-    var zoom = parseInt(Page.style.zoom) + 10 +'%'
+const zoomIn = () => {
+    let Page = document.getElementById('Body');
+    let zoom = parseInt(Page.style.zoom) + 10 +'%'
     Page.style.zoom = zoom;
 
-    var pageZoomRate = document.getElementById('zoomRate');
+    let pageZoomRate = document.getElementById('zoomRate');
     zoomRate += 10;
     pageZoomRate.innerHTML = `${zoomRate}%`;
 
     return false;
 }
 
-function zoomOut() {
-    var Page = document.getElementById('Body');
-    var zoom = parseInt(Page.style.zoom) - 10 +'%'
+const zoomOut = () => {
+    let Page = document.getElementById('Body');
+    let zoom = parseInt(Page.style.zoom) - 10 +'%'
     Page.style.zoom = zoom;
 
-    var pageZoomRate = document.getElementById('zoomRate');
+    let pageZoomRate = document.getElementById('zoomRate');
     zoomRate -= 10;
     pageZoomRate.innerHTML = `${zoomRate}%`;
 
     return false;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('zoomIn');
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-        zoomIn();
-    });
-});
+document.addEventListener("DOMContentLoaded", () => {
+    let d = new Date();
+    let date =  `${String(d.getFullYear())}-${String(d.getMonth()+1)}-${String(d.getDate())}`;
+    loadMeal(date);
 
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('zoomOut');
-    // onClick's logic below:
-    link.addEventListener('click', function() {
-        zoomOut();
+    document.getElementById('zoomOut').addEventListener('click', zoomOut);
+    document.getElementById('zoomIn').addEventListener('click', zoomIn);
+    document.getElementById('prevDate').addEventListener('click', () => {
+        d.setDate(d.getDate() - 1); 
+        let date =  `${String(d.getFullYear())}-${String(d.getMonth()+1)}-${String(d.getDate())}`;
+        loadMeal(date);
     });
-});
+    
+    document.getElementById('nextDate').addEventListener('click', () => {
+        d.setDate(d.getDate() + 1); 
+        let date =  `${String(d.getFullYear())}-${String(d.getMonth()+1)}-${String(d.getDate())}`;
+        loadMeal(date);
+    });
+})
